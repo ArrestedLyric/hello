93a4d807c32b5a540177debe0a47f79a5aad0b44
Jordan McDowell <jmcdowell22@huskers.unl.edu>
2020-08-20 02:49:50 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,14 @@
+/**
+ * Author: Jordan McDowell
+ * Date: 2020/08/19
+ * 
+ * A simple hello world program
+ */ 
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    
+    printf("Hello world!\n");
+    
+}
